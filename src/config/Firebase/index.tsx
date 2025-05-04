9b6fb77c78fcaccb0d9a8a70a88770b13ac97f3a
Novendrakailas <s22210223@student.unklab.ac.id>
2025-05-04 21:49:14 +0800

    feat: firebase config

---
+++ src/config/Firebase/index.tsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC-7aM0QtBHJbhJl6cqZZXxJG65l0kWO1Y",
+  authDomain: "cozykost-4f305.firebaseapp.com",
+  projectId: "cozykost-4f305",
+  storageBucket: "cozykost-4f305.firebasestorage.app",
+  messagingSenderId: "770469474304",
+  appId: "1:770469474304:web:959dc93b0eb3fa3d8d68e5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
