9ec99a8c2a4546b5c32bbca09c31522237b1e490
gabrielntl <s22210085@student.unklab.ac.id>
2025-05-07 01:55:23 +0800

    adding: auth

--- src/config/Firebase/index.tsx
+++ src/config/Firebase/index.tsx
@@ -1,30 +1,22 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { initializeAuth, getReactNativePersistence } from "firebase/auth";
-import { getFirestore } from "firebase/firestore"; // Tambahkan ini
-import AsyncStorage from "@react-native-async-storage/async-storage";
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getDatabase } from 'firebase/database';
+
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC-7aM0QtBHJbhJl6cqZZXxJG65l0kWO1Y",
-  authDomain: "cozykost-4f305.firebaseapp.com",
-  projectId: "cozykost-4f305",
-  storageBucket: "cozykost-4f305.firebasestorage.app",
-  messagingSenderId: "770469474304",
-  appId: "1:770469474304:web:959dc93b0eb3fa3d8d68e5"
-};
+    apiKey: "AIzaSyC-7aM0QtBHJbhJl6cqZZXxJG65l0kWO1Y",
+    authDomain: "cozykost-4f305.firebaseapp.com",
+    projectId: "cozykost-4f305",
+    storageBucket: "cozykost-4f305.firebasestorage.app",
+    messagingSenderId: "770469474304",
+    appId: "1:770469474304:web:959dc93b0eb3fa3d8d68e5"
+  };
 
-// Initialize Firebase
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const database = getDatabase(app);
 
-// Initialize Auth with AsyncStorage
-const auth = initializeAuth(app, {
-  persistence: getReactNativePersistence(AsyncStorage),
-});
+export default app;
 
-// Initialize Firestore
-const firestore = getFirestore(app); // Tambahkan inisialisasi Firestore
-
-export { app, auth, firestore };
